f46ba06c105f72a66a78ad3041f3d375626aec78
A.Islamshin <arshavin0511@gmail.com>
2022-12-05 15:22:49 +0300

    main

---
+++ main.py
@@ -0,0 +1,8 @@
+# from Func.poly_func import polynomial
+#
+# print(polynomial(4))
+# data = open('pol1.txt', 'w')  # генерируем и записываем в файлы многочлен. меняем 1 и 2 по надобности
+# data.write(polynomial(4))
+#
+#
+# data.close()
